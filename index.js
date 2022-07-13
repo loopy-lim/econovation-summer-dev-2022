c5b50a8b973521879b956267e71765c9736c248a
loopy-lim <loopy@jnu.ac.kr>
2022-07-13 18:10:57 +0900

    feat: add hanburgerbar with spin

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
-new fullpage("#fullpage", {
-  licenseKey: "",
-  anchors: [
-    "firstPage",
-    "secondPage",
-    "thirdPage",
-    "fourthPage",
-    "fifthPage",
-    "sixthPage",
-  ],
-  sectionsColor: ["#f2f2f2", "#4BBFC3", "#7BAABE", "whitesmoke", "#000"],
+function buttonBind() {
+  // Category Section
+  $("#category-button").on("click", () => {
+    if (isCategoryButtonActive) {
+      $("#category-button").addClass("is-active");
+    } else {
+      $("#category-button").removeClass("is-active");
+    }
+    isCategoryButtonActive = !isCategoryButtonActive;
+  });
+}
 
-  autoScrolling: true,
-  scrollHorizontally: true,
-});
+function init() {
+  new fullpage("#fullpage", {
+    licenseKey: "",
+    anchors: [
+      "firstPage",
+      "secondPage",
+      "thirdPage",
+      "fourthPage",
+      "fifthPage",
+      "sixthPage",
+    ],
+    sectionsColor: ["#f2f2f2", "#4BBFC3", "#7BAABE", "whitesmoke", "#000"],
 
-document.getElementsByClassName("fp-watermark")[0].remove();
-new Array(...document.getElementsByClassName("fp-arrow")).forEach((fpArrow) => {
-  fpArrow.remove();
-});
+    autoScrolling: true,
+    scrollHorizontally: true,
+  });
+
+  document.getElementsByClassName("fp-watermark")[0].remove();
+  new Array(...document.getElementsByClassName("fp-arrow")).forEach(
+    (fpArrow) => {
+      fpArrow.remove();
+    }
+  );
+  buttonBind();
+}
+let = isCategoryButtonActive = true;
+
+init();
