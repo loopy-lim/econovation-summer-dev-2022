ac31f108c9b4a513e254cddf22a7aded77757be7
loopy-lim <loopy@jnu.ac.kr>
2022-07-13 16:29:58 +0900

    feat: add fullpage.js

---
+++ index.js
@@ -0,0 +1,14 @@
+new fullpage("#fullpage", {
+  anchors: [
+    "firstPage",
+    "secondPage",
+    "thirdPage",
+    "fourthPage",
+    "fifthPage",
+    "sixthPage",
+  ],
+  sectionsColor: ["#f2f2f2", "#4BBFC3", "#7BAABE", "whitesmoke", "#000"],
+
+  autoScrolling: true,
+  scrollHorizontally: true,
+});
